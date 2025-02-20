839781545c7bc67c3ed8475f97cee451f8d2ad02
Saha1999 <aleksandraluchcko05@mail.com>
2025-02-20 09:28:03 +0300

    dдомашка

--- script.js
+++ script.js
@@ -37,24 +37,25 @@ switch (day) {
     case 12:
         case 1:
         case 2:
-            alert = ('Зима');
+            alert ('Зима');
             break;
         case 3:
         case 4:
         case 5:
-            alert = ('Весна');
+            alert ('Весна');
             break;
         case 6:
         case 7:
         case 8:
-            alert = ('Лето');
+            alert ('Лето');
             break;
         case 9:
         case 10:
         case 11:
-            alert = ('Осень');
+            alert ('Осень');
             break;
         default:
-            alert = ('Неизвестный месяц'); 
+            alert ('Неизвестный месяц'); 
+            
             
     }
